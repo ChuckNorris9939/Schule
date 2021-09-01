6aceb787a88054b111f3164886aff955afd286e6
chris <chriska97@gmail.com>
2021-09-01 12:13:38 +0200

    a

--- main.py
+++ main.py
@@ -1,16 +1,99 @@
-# This is a sample Python script.
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import random
+
+def schaltjahr():
+
+    jahr = int(input("Geben Sie ein Schaltjahr an: "))
+
+    if jahr % 400 == 0:
+        print("Es ist ein Schaltjahr")
+    elif jahr % 100 == 0:
+        print("Es ist kein Schaltjahr")
+    elif jahr % 4 == 0:
+        print("Es ist ein Schaltjahr")
+    elif jahr == 0:
+        sys.exit()
+    else:
+        print("Es ist kein Schaltjahr")
+
+    schaltjahr()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def hundejahre():
+
+    # Landläufig rechnet man Hundejahre in Menschenjahre um, indem man das Alter des Hundes mit 7 multipliziert. Je nach Hundegröße und Rasse sieht die Umrechnung jedoch etwas komplizierter aus, z.B.:
+    # Ein einjähriger Hund entspricht in etwa einem 14-jährigen Menschen.
+    # 2 Jahre eines Hundes entsprechen 22 Jahre eines Menschen.
+    # Ab dann entspricht ein Hundejahr jeweils 5 Menschenjahren.
+
+    jahr = int(input("Geben Sie ein Hundejahr an: "))
+
+    ergebnis = ""
+    if jahr == 1:
+        ergebnis = 14
+    elif jahr == 2:
+        ergebnis = 22
+    elif jahr == 3:
+        ergebnis = jahr * 5
+
+    print("Der Hund ist " + ergebnis + " Jahre als")
+
+    hundejahre()
 
 
-# Press the green button in the gutter to run the script.
+def forschleife():
+
+    endzahl = int(input("Geben Sie Ein Zahl zwischen 1 - 100 "))
+
+    summe = 0
+    for i in range(1, endzahl):
+        summe = summe + i
+        print("summe:" + str(summe) + "i:" + str(i))
+    print(summe)
+
+    forschleife()
+
+
+def mrandom():
+
+    randomNumber = random.randint(1, 20)
+
+    userInput = int(input("Rate die Nummer zwischen 1 und 20: "))
+    versuche = 5
+    while True:
+        if versuche == 0:
+            print("Du hast keine Versuche mehr!")
+            break
+        else:
+            if userInput != randomNumber:
+                print("Falsch!. Nocheinmal")
+                userInput = int(input("Noch einmal! Du hast noch " + str(versuche) + " Versuche "))
+                versuche = versuche - 1
+                pass
+            else:
+                print("Richtig")
+                break
+
+
+def mrandom2():
+
+    randomNumber = random.randint(1, 20)
+    print(randomNumber)
+
+    userInput = int(input("Rate die Nummer zwischen 1 und 20: "))
+    versuche = 5
+    while versuche != 0:
+        if userInput != randomNumber:
+            print("Falsch!. Nocheinmal")
+            userInput = int(input("Noch einmal! Du hast noch " + str(versuche) + " Versuche "))
+            versuche = versuche - 1
+            pass
+        else:
+            print("Richtig")
+            break
+    print("Du hast keine Versuche mehr!")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mrandom()
